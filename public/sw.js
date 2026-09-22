fe6dc69d54e64c184dbfbdc40a99b25ce7660d81
Mohammad <hatamimohammad78@gmail.com>
2026-09-23 01:54:45 +0330

    Add PWA support

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "jahan-hasti-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
